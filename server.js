7ae7afee343886673c3301cf72314b22ddbf6356
Nicholas <nicholaspriegel@gmail.com>
2024-04-26 11:24:02 -0400

    created routes, views, partials

--- server.js
+++ server.js
@@ -7,26 +7,50 @@ require('dotenv').config()
 const methodOverride = require('method-override')
 const port = 3000
 
-
 //----------------------------Middleware------------------------------------------
 
 app.use(express.urlencoded({extended: false})) // CHECK
-app.use(methodOverride()) // CHECK
+app.use(methodOverride("_method")) // CHECK
 
 //----------------------------DB Connection------------------------------------------
+
 mongoose.connect(process.env.MONGODB_URI)
-mongoose.connection.on('true', ()=>{
-    console.log('Connected to MongoDB');
+mongoose.connection.on('connected', ()=>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 })
+
 //----------------------------Routes------------------------------------------------
 
 // get homepage
+app.get('/blog', (req, res)=>{
+    res.render('index.ejs', {
+        title: "Nick's Blog 2: Homepage"
+    })
+})
+
 // get create page
+app.get('/blog/new', (req, res)=>{
+})
+
 // post input to db
+app.post('/blog', (req, res)=>{
+})
+
 // get show page
+app.get('/blog/:id', (req, res)=>{
+})
+
 // delete from show page
+app.delete('/blog/:id', (req, res)=>{
+})
+
 // get edit page
-// post edit to db
+app.get('/blog/:id/edit', (req, res)=>{
+})
+
+// put edit to db
+app.put('/blog/:id', (req, res)=>{
+})
 
 //----------------------------Port--------------------------------------------------
 
